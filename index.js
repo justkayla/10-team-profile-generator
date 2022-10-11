791733a317acfc270ce793a6f79539aee3833c82
Kayla Justice <kaylarjustice@gmail.com>
2022-10-11 16:36:39 -0500

    class organization and initial inquirer work.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const employee = require('./lib/employee');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
-const manager = require('./lib/manager');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
 const generateHTML = require('./lib/htmldata');
+
+const engineer1 = new Engineer ('kayla', 12, 'kayla@gmail', 'justkayla')
+console.log(engineer1)
+
+const intern1 = new Intern ('kayla', 12, 'kayla@gmail', 'justkayla')
+console.log(intern1)
+
+const manager1 = new Manager ('kayla', 12, 'kayla@gmail', 'justkayla')
+console.log(manager1)
+
+
 
