11926313081d00a630eba10215858a948b923f40
Kayla Justice <kaylarjustice@gmail.com>
2022-10-11 17:08:22 -0500

    inquirer functions for each employee type added.

--- index.js
+++ index.js
@@ -4,16 +4,85 @@ const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
-const generateHTML = require('./lib/htmldata');
+// const generateHTML = require('./lib/htmldata');
 
-const engineer1 = new Engineer ('kayla', 12, 'kayla@gmail', 'justkayla')
-console.log(engineer1)
 
-const intern1 = new Intern ('kayla', 12, 'kayla@gmail', 'justkayla')
-console.log(intern1)
+// Promise?
+async function employeeData() {
+    return inquirer
+        .prompt([
+            {
+              type: 'list',
+              message: 'What type of employee?',
+              name: 'employeetype',
+              choices: [
+                'manager',
+                'engineer',
+                'intern',
+                ]
+            },
+            {
+                type: 'input',
+                message: 'What is the employee name?',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'What is the employee id?',
+                name: 'employeeid',
+            },
+            {
+                type: 'input',
+                message: 'What is the email address?',
+                name: 'email',
+            }            
+        ])
+}
 
-const manager1 = new Manager ('kayla', 12, 'kayla@gmail', 'justkayla')
-console.log(manager1)
+function managerData() {
+    return inquirer
+        .prompt([
+        {   type: 'input',
+            message: 'What is the office number?',
+            name: 'officenum'
+        }
+    ])         
+}
+
+function engineerData() {
+    return inquirer
+        .prompt([
+        {   type: 'input',
+            message: 'What is your GitHub?',
+            name: 'github'
+        }
+    ])         
+}
+
+function internData() {
+    return inquirer
+        .prompt([
+        {   type: 'input',
+            message: 'What is your school?',
+            name: 'school'
+        }
+    ]) 
+}
+
+// Can use switch here instead of else/if
+async function init() {
+    let data = await employeeData();
+
+    if(data.employeetype === "manager") {
+        managerData();
+    } else if(data.employeetype === "engineer") {        
+        engineerData();
+    } else {
+        internData();
+    }
+}
+
+init();
 
 
 
