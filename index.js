dc5c983e15137febcb623f5e671b2e09a1f8979c
Kayla Justice <kaylarjustice@gmail.com>
2022-10-11 15:37:08 -0500

    js file structuring.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const employeeData = require('./lib/employee');
-const engineerData = require('./lib/engineer');
-const internData = require('./lib/intern');
-const managerData = require('./lib/manager');
+const employee = require('./lib/employee');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+const manager = require('./lib/manager');
 const generateHTML = require('./lib/htmldata');
 
