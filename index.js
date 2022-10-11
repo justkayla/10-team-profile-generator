1b9f8284e617aa46f3be1f02c0e94df2123b61a7
Kayla Justice <kaylarjustice@gmail.com>
2022-10-11 15:34:31 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const employeeData = require('./lib/employee');
+const engineerData = require('./lib/engineer');
+const internData = require('./lib/intern');
+const managerData = require('./lib/manager');
+const generateHTML = require('./lib/htmldata');
+
