4259cad71966b93a8f4b906c8c9ac2358d9509c3
Kayla Justice <kaylarjustice@gmail.com>
2022-10-12 10:05:45 -0500

    Reorg of the main index file.

--- index.js
+++ index.js
@@ -4,85 +4,9 @@ const Employee = require('./lib/employee');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
-// const generateHTML = require('./lib/htmldata');
+const {generateHTML, employeeData, managerData, engineerData, internData, askAgain, writeToFile} = require('./lib/htmldata');
 
 
-// Collaborated on section w/ Tarek M, Tyler O, Alex O, and Hunter A
-// async functions to controll questions based on employee type
-async function employeeData() {
-    return inquirer
-        .prompt([
-        {
-            type: 'list',
-            message: 'What type of employee?',
-            name: 'employeetype',
-            choices: [
-            'manager',
-            'engineer',
-            'intern',
-            ]
-        },
-        {
-            type: 'input',
-            message: 'What is the employee name?',
-            name: 'name',
-        },
-        {
-            type: 'input',
-            message: 'What is the employee id?',
-            name: 'employeeid',
-        },
-        {
-            type: 'input',
-            message: 'What is the email address?',
-            name: 'email',
-        }            
-    ])
-}
-
-// If manager was selected, ask this additional question
-function managerData() {
-    return inquirer
-        .prompt([
-        {   type: 'input',
-            message: 'What is the office number?',
-            name: 'officenum',
-        }
-    ])         
-}
-
-// If engineer was selected, ask this additional question
-function engineerData() {
-    return inquirer
-        .prompt([
-        {   type: 'input',
-            message: 'What is your GitHub?',
-            name: 'github',
-        }
-    ])         
-}
-
-// If intern was selected, ask this additional question
-function internData() {
-    return inquirer
-        .prompt([
-        {   type: 'input',
-            message: 'What is your school?',
-            name: 'school',
-        }
-    ]) 
-}
-
-// Ask to add another employee (confirm Y/n = true/false)
-async function askAgain() {
-    return inquirer
-        .prompt([
-        {   type: 'confirm',
-            message: 'Do you want to add another employee?',
-            name: 'addanother',
-        }
-    ]) 
-}
 
 // Can use switch here instead of else/if
 async function init() {
@@ -91,21 +15,30 @@ async function init() {
     
     while(addEmployee) {
         let data = await employeeData();
+        console.log(data)
 
         if(data.employeetype === "manager") {
             await managerData();    // Wait for response before engaging askAgain()
+            data = {...data, ...x} // spread syntax
+            console.log(data) // Data is an object, managerData is an object data = {...data, ...x} spread syntax
+            // push data to array
         } else if(data.employeetype === "engineer") {        
             await engineerData();   // Wait for response before engaging askAgain()
+            // push data to array
         } else {
             await internData();     // Wait for response before engaging askAgain()
+            // push data to array
         }
         
         let result = await askAgain();
 
-        if(!result.confirm) {
+        if(!result.addanother) {
             addEmployee = false
         }
     }
+
+    // Empty array to push stuff into to pull from for writeToFile
+    writeToFile
 }
 
 init();
