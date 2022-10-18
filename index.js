c0a93d8cec89352cd39643dceb4b7c9c71aa1bdd
Kayla Justice <kaylarjustice@gmail.com>
2022-10-17 22:53:08 -0500

    Messing around with HTML.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const generateHTML = (employees) => {
         if(employees[i].getRole() === "Manager") {
             special = "Office number: "+employees[i].getOfficeNum()
         } else if (employees[i].getRole() === "Engineer"){
-            special = "GitHub: "+employees[i].getGithub()       
+            special = "GitHub: "+"github.com/"+employees[i].getGithub()       
         } else {
             special = "School: "+employees[i].getSchool()
         }
@@ -29,7 +29,7 @@ const generateHTML = (employees) => {
                     <ul class="list-group">
                         <li class="list-group-item">Name: ${employees[i].getName()}</li>
                         <li class="list-group-item">Employee ID: ${employees[i].getId()}</li>
-                        <li class="list-group-item">Email: ${employees[i].getEmail()}</li>                    
+                        <li class="list-group-item">Email: <a href = "mailto: ${employees[i].getEmail()}"></a></li>                    
                         <li class="list-group-item">${special}</li>                    
                     </ul>
                 </div>            
